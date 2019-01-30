17d946b7fbbe3958a95be1d36197657b8260b556
ignaciorod1 <ignacio.rodriguez.naranjo@alumnos.upm.es>
2019-01-30 01:25:55 +0100

    better homing :)

--- PID-control.ino
+++ PID-control.ino
@@ -11,17 +11,32 @@ encoder enc;
 motor mot;
 
 volatile bool end_flag = false;
-
+bool r_flag = false;
+bool init_flag = false;
+int i = 0;
+int readVal = 0;
 
 void endstopPressed(){  //ISR function
   end_flag = true;  
 }
 
 void homing(){
-  if(end_flag)
-      mot.stops();
-  else
-    mot.setPwm(75);
+  if(end_flag){
+      if(r_flag){
+        mot.stops();
+        init_flag = true;
+      }
+      else{
+        if(i == 1000)  r_flag = true;
+        else{
+          mot.setDir(1);
+          enc.setDir(1);
+          i++;
+        }
+      }     
+  }
+  else{;}
+    
 }
 
 void setup() {
@@ -32,17 +47,35 @@ void setup() {
 
   enc.setPin(enc_pin);
   mot.setPins(pwm_pin, in1_pin, in2_pin);
-  mot.setDir(1);
-  enc.setDir(1);
+  mot.setDir(0);
+  enc.setDir(0);
+  mot.setPwm(75);
 }
 
 void loop() {
 
   enc.measure();
-  enc.printLaps(); 
-  enc.printDir();
+  //enc.printLaps(); 
+  //enc.printDir();
   
-  homing();
+  if(!init_flag)
+    homing();
+ Serial.print(i);
+ Serial.print("   ");
+ Serial.println(r_flag);
+/*
+  else if(init_flag){
+
+    mot.setDir(1);
+    enc.setDir(1);
+
+    readVal = (int)Serial.read();
+    
+    if (readVal >= 75 && readVal <= 255){
+     mot.setPwm(180);
+    }
+  }
+*/
 
   mot.move();
 
